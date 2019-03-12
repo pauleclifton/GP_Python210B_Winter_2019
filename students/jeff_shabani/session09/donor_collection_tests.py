9df3e804cfe2047ce579971d1a77579320ae985e
Jeff Shabani <jeffrey.shabani@outlook.com>
2019-03-12 12:50:55 -0700

    donor_models tests

--- students/jeff_shabani/session09/donor_collection_tests.py
+++ students/jeff_shabani/session09/donor_collection_tests.py
@@ -5,9 +5,10 @@
 # Python 210, Session 8
 # donor_collection_tests.py
 
+import os
 import unittest
 
-from students.jeff_shabani.session09.donor_collection import *
+from students.jeff_shabani.session09.donor_models import *
 
 ANSWER = 'New_Donor'
 AMOUNT = 4512
@@ -21,7 +22,33 @@ AMOUNT = 4512
 
 class OOMailroonTests(unittest.TestCase):
 
+    def test_add_donor(self):
+        d = Donor()
+        d.add_donor(ANSWER, AMOUNT)
+        expected = {f'{ANSWER}':[4512]}
+        self.assertDictEqual(expected, d.donors)
+        del d
+
+    def test_write_a_letter(self):
+        d = Donor()
+        """
+        test that a single letter is written, saved as a text
+        file and named correctly.
+        """
+        self.assertEqual(d.write_a_single_letter(ANSWER, AMOUNT), True)
+        del d
+        os.remove('New_Donor.txt')
+
     def test_view_donor_names(self):
+        d = Donor()
+        d.add_donor(ANSWER, AMOUNT)
+        """
+        test that function returns all donor names
+        """
+        self.assertEqual(d.view_donor_names(), print('New Donor'))
+        del d
+
+    def test_view_donor_names_multiple(self):
         dc = DonorCollection()
         """
         test that function returns all donor names
