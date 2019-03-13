679370598d8284d615bb5277ed582d3bc18561c9
JRockwell70 <rockwellshabani@gmail.com>
2019-03-12 17:02:13 -0700

    cli_main update

---
+++ students/jeff_shabani/session09/cli_main_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import unittest
+
+from cli_main import *
+
+
+
+class OOMailroonTests(unittest.TestCase):
+    pass
+
+    def test_get_value(self):
+        #requires tester input
+        clm = CommandLineInterface()
+        expected = 10
+        self.assertEqual(clm.get_value('Enter 10', int), expected)
+        del clm
+
+    def test_set_letter_dir_path(self):
+        #requires tester input
+        clm = CommandLineInterface()
+        expected = os.getcwd()
+        self.assertEqual(clm.set_letter_directory_path_path(), expected)
+        del clm
+
