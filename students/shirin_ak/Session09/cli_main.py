5c6f2d557cc70d7f40c0467cca77061041901204
Mahmood08 <tashi_99@yahoo.com>
2019-03-16 15:23:08 -0700

    updated files

--- students/shirin_ak/Session09/cli_main.py
+++ students/shirin_ak/Session09/cli_main.py
@@ -1,3 +1,95 @@
 
+import sys
+import math
+from donor_models import Donor
+from donor_models import DonorCollection
 
-"""This is the command line interface for the mailroom."""
+"""This is the command line interface for the mailroom.
+   All the input()function calls and print() function will
+   include in this class. In other words this class will
+   handle each of mode of the program
+ """
+
+
+dc = DonorCollection()
+
+
+class CommandInterface():
+    PROMPT = "\n".join(("Please select a menu option below:",
+                    "1 - Send a Thank You to an individual",
+                    "2 - Create a Report",
+                    "3 - Send letters to all donors",
+                    "4 - Quit",
+                    ">>> "))
+
+
+
+    def thank_you_message():
+         while True:                         
+             name = input("Enter 'name' to add new donor\n"
+                         " or nter 'list' to see list of donors\n"
+                         " (or type 'menu to exit)>>").strip()             
+
+             if name == 'list':                
+                print(dc.donor_list())
+             elif name == 'menu':
+                 return
+             else:
+
+                break
+
+         while True:
+             donation_input = input("Enter the amount donated "
+                              "(or 'menu' to exit)>").strip()
+             if donation_input == 'menu':
+
+                 return
+             try:
+               amount = float(donation_input)
+               return donation_input
+             except ValueError:
+               print("please enter a valid donation amount")
+
+             else:
+                 break
+         # If this is a new user, ensure that the database has the necessary
+         # data structure.                   
+         donor = dc.find_donor(name)
+         if donor is None:
+             donor = dc.add_donor(name)
+         #record the donation    
+             donor.add_donation(amount)
+             print(dc.write_letter(donor))
+
+
+def print_donor_report():
+       print(dc.donor_report())
+
+       
+def exit_program():
+    print("Bye!")
+    sys.exit(0)
+
+    
+def runmain():
+    
+    selection_dict = {"1": CommandInterface.thank_you_message,
+                     "2": print_donor_report,
+                     "3": dc.save_letters_to_disk,
+                     "4": exit_program
+                      }
+
+    while True:
+        selection = input(CommandInterface.PROMPT)
+        try:
+            selection_dict[selection]()
+        except KeyError:
+            print("error: menu selection is invalid!")
+
+
+if __name__ == "__main__":
+
+    runmain()
+
+    
+        
--- students/shirin_ak/Session09/donor_models.py
+++ students/shirin_ak/Session09/donor_models.py
@@ -3,7 +3,8 @@ import string
 
 #This class will hold all the information about a single donor,
 #and have attributes, properties, and methods to provide access
-#to the donor-specific information. 
+#to the donor-specific information.
+
 class Donor():
     """Donor Class"""
     def __init__(self, name):
@@ -45,5 +46,101 @@ class Donor():
         with the donor's name and last donation amount.
         """
         return "Dear {:s},\n\nWe greatly appreciate your generous donation of"
-        " ${:,.2f}.\n\nThank you,\nThe Team".format(self._name, self._donations[-1])
+        " ${:,.2f}.\n\nThank you,\nThe Team".format(self.name, self.donations[-1])
+        
+ #--------------------------------
+ #       End of Donor class
+ #------------------------------
+
+# This class will hold all of the donor objects, as well as methods to add a new donor,
+#search for a given donor,  generates reports about multiple donors.
+#In other words,if the functionality involves more than one donor – it belongs in this class.
     
+class DonorCollection():
+    def __init__(self):
+        self.donors_dict = {'william gates':[3, 3000.45],
+                            'Jeff Bezos':[2, 3452.00],
+                            }
+            
+        
+            
+           
+    def find_donor(self, name):
+        if name.lower() in self.donors_dict:
+            return self.donors_dict[name.lower()]
+        return None
+
+
+    def add_donor(self, name):
+        """allows for adding new donors to the db"""
+        
+        new_donor = Donor(name)
+        self.donors_dict[new_donor.name.lower()] = new_donor
+        return new_donor
+
+    
+    @staticmethod
+    def sort_key(item):
+        return item[1]
+
+    
+    def donor_list(self):
+        name_list = [donor.name for donor in self.donors_dict]
+        return '\n'.join(name_list)
+
+
+#Builds the donor report and displays the name, total given, number of gifts
+# and the average gift donated by the donor.
+
+    def donor_report(self):
+        report_rows = []
+        for donor in self.donors_dict:
+            name = donor.name
+            donations = donor.donations
+            total_donations = donor.total_donations
+            num_donations = len(donations)
+
+            avg_donation = donor.average_donation
+            report_rows.append((name, total_donations, num_donations, avg_donation))
+        report_rows.sort(key=self.sort_key)
+        report = []
+
+        report.append("{:25s} | {:11s} | {:9s} | {:12s}".format("Donor Name",
+                                                                "Total Donated",
+                                                                "Num gifts",
+                                                                "Average Donation"))
+
+        report.append("_" * 66)
+        for row in report_rows:
+            report.append("{:25s}   ${:10.2f}   ${:9d}   ${:11.2f}".format(*row))
+        return "\n".join(report)
+
+    
+    def write_letter(self, donor):
+        """
+        returns a string that is a formatted thank you note
+        with the donor's name and last donation amount.
+        """               
+        return ("Dear {:s},\n\nWe greatly appreciate your generous donation of"
+        " ${:,.2f}.\n\nThank you,\nThe Team".format(donor.name, donor.donations[-1]))
+
+    
+    def save_letters_to_disk(self):
+        
+         """
+        Generate letter for each donor and write to disk
+        """
+         for donor in self.donors_dict:
+            print("creating letter to{:s}".format(donor.name))
+            letter = self.write_letter(donor)
+            filename = donor.name.replace(" ", "_") + ".txt"
+            with open(filename, 'w') as outfile:
+                outfile.write(letter)
+
+
+
+ #----------------------------------------
+ #       End of DonorCollection class
+ #----------------------------------------
+
+
